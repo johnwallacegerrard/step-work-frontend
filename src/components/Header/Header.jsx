6b068e0840c81cd87ec6cfd4292090344ed54ebc
johnwallacegerrard <levijohnbailey@gmail.com>
2025-07-29 18:11:10 -0400

    fix header logo import and favicon link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,13 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import Logo from "./src/assets/StepWorkIcon.svg";
 
 function Header() {
   return (
     <div className="header">
       <Link to="/" className="header__logo-link">
-        <img
-          src="./src/assets/StepWorkIcon.svg"
-          alt="header-logo"
-          className="header__logo"
-        />
+        <img src={Logo} alt="header-logo" className="header__logo" />
         <h1 className="header__title">StepWork</h1>
       </Link>
     </div>
